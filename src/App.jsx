0413152c9a725180796378e16dd33e3fea0900b2
silas <silasgebhart12@gmail.com>
2024-08-24 15:10:08 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export default function App() {
+  return (
+    <div className='flex flex-col p-4'>
+      <section className='min-h-screen flex flex-col'>
+        <header>
+          <h1>Interior<span className='text-red-800'>View</span></h1>
+        </header>
+        <main className='flex-1'>
+
+        </main>
+      </section>
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
