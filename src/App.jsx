0b5c190c8fe960249745ff2729affda00daab670
silas <silasgebhart12@gmail.com>
2024-08-24 20:21:55 -0500

    Added a new div to hold small summary of project

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 
 export default function App() {
   return (
-    <div className='flex flex-col p-4'>
-      <section className='min-h-screen flex flex-col'>
-        <header>
-          <h1>Interior<span className='text-red-800'>View</span></h1>
-        </header>
-        <main className='flex-1'>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <section>
+        <header className='flex text-3xl items-center justify-center p-4'>Interior<span className='text-red-500'>View</span></header>
+        <main>
+
+          <div className='flex flex-col items-center justify-center'>
+            <h1>Welcome! I am implementing a 360 degree panoramic view for the inside of a car or helicopter.</h1>
+          </div>
 
         </main>
       </section>
       <footer>
-
+        
       </footer>
     </div>
   )
